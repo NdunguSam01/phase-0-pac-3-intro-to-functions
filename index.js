66e363a45ab3a4dcaa781939768627956f0c2f59
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-09-26 17:03:53 +0300

    Intro to JS Functions done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Follow along with the examples here
+function helloWorld()
+{
+    console.log("Hello World");
+}
+
+helloWorld(); //Function call
+
+const greet=(firstName)=>//First name acts as the function's parameter
+{
+    console.log(`Hello ${firstName}`);
+}
+greet("Samuel");//Samuel is passed onto the function as an argument during its invocation
+
+const add=(x, y)=>
+{
+    return x + y;
+}
+
+console.log("Kshs. " +add(3000,2000));
